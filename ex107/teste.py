33ef008b4f9cf096ef087809c1047690aac98e77
EddieNine <believefixed@hotmail.com>
2024-08-21 17:05:08 -0300

    Exercitando módulos em Python

--- ex107/teste.py
+++ ex107/teste.py
@@ -1,6 +1,6 @@
 import ex107
 
 preco = float(input('Digite o preço: R$'))
-print(f'A metade de R${preco:.2f} é R${ex107.aumentar(preco):.2f}')
-print(f'O dobro de R${preco:.2f} é R${ex107.dobro(preco):.2f}')
-print(f'Aumentando 10%, temos R${ex107.aumentar(preco, 10):.2f}')
+print(f'A metade de R${preco} é R${ex107.aumentar(preco)}')
+print(f'O dobro de R${preco} é R${ex107.dobro(preco)}')
+print(f'Aumentando 10%, temos R${ex107.aumentar(preco, 10)}')
